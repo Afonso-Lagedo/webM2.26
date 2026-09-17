3539b0f0e56536c08d93b4e90d85078d9d739383
Afonso-Lagedo <afonso.lagedo@gmail.com>
2026-09-17 10:44:31 -0300

    calculadora

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # webM2.26
 
 Repositório para acompanhamento das aulas da turma do segundo semestre de web
+
+
+## OBS para exercício da calculadora de soma:
+
+PROMPT: Gere apenas o html e css de uma calculadora para soma, os campos devem ser através de um formulário, irei desenvolver o js sem você usando o submit
--- main.js
+++ main.js
@@ -1,24 +1,12 @@
-/*Tipos de variáveis
-var -> escopo global
-let -> escopo local
-const -> escopo flex, valor será o mesmo até o fim do programa
-*/
+const formulario = document.getElementById("calcForm")/*pegando o formulário*/
 
-/*Criando variável*/
-var valorUm = 3
-var valorDois = 19
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();/*impede que a página seja recarregada*/ 
 
-/*
-espremedor (fruta){
-    suco
-}
-*/
+    const a = Number(document.getElementById("num1").value)
+    const b = Number(document.getElementById("num2").value)
 
-/*função*/
-function somar(a,b){
-    let c = a+b
-    return (c)
-}
+    const soma = a+b
 
-console.log(somar(valorUm,valorDois))
-
+    document.getElementById("resultado").textContent = soma
+})
