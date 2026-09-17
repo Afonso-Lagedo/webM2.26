a158636248076c6236e7e845735d82b91f0075e8
Afonso-Lagedo <afonso.lagedo@gmail.com>
2026-09-17 09:22:59 -0300

    função

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-/*Criando variável*/
-var a = 5
+/*Tipos de variáveis
+var -> escopo global
+let -> escopo local
+const -> escopo flex, valor será o mesmo até o fim do programa
+*/
 
-/*Exibindo valor da variável*/
-console.log(a)
+/*Criando variável*/
+var valorUm = 3
+var valorDois = 19
+
+/*
+espremedor (fruta){
+    suco
+}
+*/
+
+/*função*/
+function somar(a,b){
+    let c = a+b
+    return (c)
+}
+
+console.log(somar(valorUm,valorDois))
+
